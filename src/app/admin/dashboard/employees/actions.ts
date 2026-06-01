8893e12895849e601c7fa6b00a38cc7cd0d5b6bd
anggisatwr <satriawiranata949@gmail.com>
2026-06-01 11:30:31 +0700

    fix: replace bcrypt with bcryptjs for Vercel edge compatibility

--- src/app/admin/dashboard/employees/actions.ts
+++ src/app/admin/dashboard/employees/actions.ts
@@ -1,6 +1,6 @@
 "use server";
 import { prisma } from "@/lib/prisma";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import { revalidatePath } from "next/cache";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
