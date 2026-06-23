15394cb314a3b4319d9a16af900e39b9be2a8c9b
anggisatwr <satriawiranata949@gmail.com>
2026-06-23 14:57:16 +0700

    Remove 95k addition from base gold price

--- src/lib/price.ts
+++ src/lib/price.ts
@@ -26,7 +26,7 @@ export async function getOrSyncDailyPrice() {
 
       if (data.price) {
         const pricePerOunce = data.price;
-        const basePricePerGram = (pricePerOunce / TROY_OUNCE_TO_GRAM) + 95000; // Tambah paten 95rb
+        const basePricePerGram = (pricePerOunce / TROY_OUNCE_TO_GRAM); // Tanpa tambahan paten
 
         // Spread/Margin
         // Harga Jual (Pelanggan beli): Base price + 3%
