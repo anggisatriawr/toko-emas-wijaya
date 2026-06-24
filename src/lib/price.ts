303634f8e39b6b84b524a949a3df049c423d0a71
anggisatwr <satriawiranata949@gmail.com>
2026-06-24 11:50:24 +0700

    Change addition back to 125k for base gold price

--- src/lib/price.ts
+++ src/lib/price.ts
@@ -26,7 +26,7 @@ export async function getOrSyncDailyPrice() {
 
       if (data.price) {
         const pricePerOunce = data.price;
-        const basePricePerGram = (pricePerOunce / TROY_OUNCE_TO_GRAM) + 135000; // Tambah paten 135rb
+        const basePricePerGram = (pricePerOunce / TROY_OUNCE_TO_GRAM) + 125000; // Tambah paten 125rb
 
         // Spread/Margin
         // Harga Jual (Pelanggan beli): Base price + 3%
